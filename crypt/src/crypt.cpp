6dff5ff952d5570192800ec222833f9bfb366989
d954mas <d954mas@gmail.com>
2023-07-14 17:11:36 +0300

    add encrypt_xtea,decrypt_xtea functions

--- crypt/src/crypt.cpp
+++ crypt/src/crypt.cpp
@@ -106,6 +106,62 @@ static int Crypt_Base64Decode(lua_State* L)
     return 1;
 }
 
+static int Crypt_EncryptXTEA(lua_State* L){
+    DM_LUA_STACK_CHECK(L, 1);
+
+    size_t srclen;
+    const char* src = luaL_checklstring(L, 1, &srclen);
+
+    size_t keylen;
+    const char* key = luaL_checklstring(L, 2, &keylen);
+    if(keylen>16){
+      luaL_error(L, "key should be <=16");
+    }
+
+    uint32_t dstlen = srclen;
+    char* dst = (char*) malloc(dstlen);
+    memcpy(dst, src, dstlen);
+    dmCrypt::Result result = dmCrypt::Encrypt(dmCrypt::ALGORITHM_XTEA, (uint8_t*)dst, dstlen, (uint8_t*)key, keylen);
+    if(result != dmCrypt::RESULT_OK){
+        free(dst);
+        luaL_error(L, "error when encrypt");
+    }else{
+        lua_pushlstring(L, dst, dstlen);
+        free(dst);
+    }
+
+    return 1;
+}
+
+static int Crypt_DecryptXTEA(lua_State* L){
+    DM_LUA_STACK_CHECK(L, 1);
+
+    size_t srclen;
+    const char* src = luaL_checklstring(L, 1, &srclen);
+
+    size_t keylen;
+    const char* key = luaL_checklstring(L, 2, &keylen);
+
+    if(keylen>16){
+      luaL_error(L, "key should be <=16");
+    }
+
+    uint32_t dstlen = srclen;
+    char* dst = (char*) malloc(dstlen);
+    memcpy(dst, src, dstlen);
+
+    dmCrypt::Result result = dmCrypt::Decrypt(dmCrypt::ALGORITHM_XTEA, (uint8_t*)dst, dstlen, (uint8_t*)key, keylen);
+    if(result != dmCrypt::RESULT_OK){
+        free(dst);
+        luaL_error(L, "error when decrypt");
+    }else{
+        lua_pushlstring(L, (char*)dst, dstlen);
+        free(dst);
+    }
+
+    return 1;
+}
+
 static const luaL_reg Module_methods[] =
 {
     {"hash_sha1", Crypt_HashSha1},
@@ -114,6 +170,8 @@ static const luaL_reg Module_methods[] =
     {"hash_md5", Crypt_HashMd5},
     {"encode_base64", Crypt_Base64Encode},
     {"decode_base64", Crypt_Base64Decode},
+    {"encrypt_xtea", Crypt_EncryptXTEA},
+    {"decrypt_xtea", Crypt_DecryptXTEA},
     {0, 0}
 };
 
